2552d366c602c87ea92a818b2ff435e9523af75a
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-06-10 09:18:40 -0400

    added index.html

--- src/test/java/io/github/JDSalisbury/ims/InventoryItemTest.java
+++ src/test/java/io/github/JDSalisbury/ims/InventoryItemTest.java
@@ -9,7 +9,7 @@ public class InventoryItemTest {
 
 	@Test
 	public void shouldReturnItemName() {
-		InventoryItem underTest = new InventoryItem("1234E", 5, 06 / 07 / 2018, "Apple", "EACH", "Aisle 10", .59,
+		InventoryItem underTest = new InventoryItem("1234E", 5, "06/07/2018", "Apple", "EACH", "Aisle 10", .59,
 				"Granny Smith");
 
 		String itemName = underTest.getName();
