3ae74d1a82c462c3aaf045785b8533976c81f569
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-06-28 19:33:29 -0400

    so I didn't have a getId(), I have one now. Also, deleteItem.js needed to grab all the buttons making a collection then going through the collection and giving them all the delete method

--- src/main/resources/static/js/addItem.js
+++ src/main/resources/static/js/addItem.js
@@ -27,30 +27,4 @@ function addItem(event){
     xhr.send()
 }
 
-deleteItem
-document.addEventListener("DOMContentLoaded", function() {
-    const deleteItemButton = document.querySelector('#deleteItem');
-    deleteItemButton.addEventListener('click', deleteItem)
-});
-
-function deleteItem(event) {
-    event.preventDefault();//prevents forms from refreshing
-	const theButton = event.target
-	const theDivWrappingTheButton = theButton.parentElement
-	console.log(theButton)
-	const id = theButton.dataset.id
-	// do ajax request to /remove-tag?id={id}&bookReviewTitle={title}
-	const xhr = new XMLHttpRequest()//ajax request 
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			console.log(xhr.response);
-//			
-//			// delete the tag from DOM
-			theDivWrappingTheButton.parentElement.removeChild(theDivWrappingTheButton)
-		}
-	}
-	xhr.open('DELETE', '/deleteTheItem' + encodeURI(id),true)
-	//id and title are retrieved with the button press
-	xhr.send()
-}
 
---
+++ src/main/resources/static/js/deleteItem.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const deleteItemButtonsCollection = document.querySelectorAll('#deleteItem');
+    deleteItemButtonList = Array.from(deleteItemButtonsCollection)
+	deleteItemButtonList.forEach( function(deleteItemButton){
+		deleteItemButton.addEventListener('click', deleteItem)
+});
+
+function deleteItem(event) {
+    event.preventDefault();//prevents forms from refreshing
+	const theButton = event.target
+	const theDivWrappingTheButton = theButton.parentElement
+	console.log(theButton)
+	const id = theButton.dataset.id
+	// do ajax request to /remove-tag?id={id}&bookReviewTitle={title}
+	const xhr = new XMLHttpRequest()//ajax request 
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr.response);
+            console.log(id);			
+			// delete the tag from DOM
+			theDivWrappingTheButton.parentElement.removeChild(theDivWrappingTheButton)
+		}
+	}
+	xhr.open('DELETE', '/deleteTheItem/' +id,true)
+	//id and title are retrieved with the button press
+	xhr.send()
+}});
