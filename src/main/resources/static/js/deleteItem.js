0e690b459f33391c664089c759848fd7c1d7557d
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-07-11 21:23:16 -0400

    Working confirm button

--- src/main/resources/static/js/deleteItem.js
+++ src/main/resources/static/js/deleteItem.js
@@ -3,10 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const deleteItemButtonsCollection = document.querySelectorAll('.deleteItem');
     deleteItemButtonList = Array.from(deleteItemButtonsCollection)
 	deleteItemButtonList.forEach( function(deleteItemButton){
-		deleteItemButton.addEventListener('click', deleteItem)
+		deleteItemButton.addEventListener('click', function (){
+		if (confirm('Are you sure you want to Delete this item?')){
+			deleteItem();
+		}})
 });
 
-function deleteItem(event) {
+
+
+function deleteItem() {
 	const theButton = event.target
 	const theDivWrappingTheButton = theButton.parentElement
 	const id = theButton.dataset.id
